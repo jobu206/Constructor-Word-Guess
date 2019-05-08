62b34f7e773b365f98916002078039225bfe7745
KevinL <mastahkevin@gmail.com>
2019-05-07 18:33:22 -0700

    added functions for guessing letter & checking guess.

--- Letter.js
+++ Letter.js
@@ -1,35 +1,20 @@
-class Letter {
-    constructor (letter) {
-        this.letter = letter;
-        this.letterGuessed = false;
-    }
-    noGuess () {
-        if (this.letterGuessed === true) {
+function Letter(letter) {
+    this.letter = letter;
+    this.letterGuessed = false;
+
+    this.getLetter = function () {
+        if (this.letterGuessed) {
             return this.letter;
         } else {
             return "_";
         }
-        // return (this.letterGuessed === true) ? this.letter : "_";
     }
-    guessedLetter (letter) {
-        if (this.letter === letter) {
+
+    this.checkGuess = function (guess) {
+        if (guess === this.letter) {
             this.letterGuessed = true;
         }
     }
-
-        
 }
-let letter = new Letter('a');
-letter.guessedLetter('a');
-console.log(letter.noGuess());
 
-let letterArr = [
-    Letter {
-        letter: "H",
-        letterGuessed: true
-    },
-    Letter {
-        letter: "o",
-        letterGuessed: false
-    }
-]
+module.exports = Letter;
