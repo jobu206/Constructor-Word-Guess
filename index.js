3d3142257f61a77f9ba0428b0227f9fff0c9cd41
KevinL <mastahkevin@gmail.com>
2019-07-29 14:15:46 -0700

    Continued buildout of CLI App.

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
 const Word = require("./Word");
+const inquirer = require ('inquirer');
 
+const letterArray = 'abcdefghijklmnopqrstuvwxyz';
+
+const mlbTrivia = [
+     'baltimore orioles',
+     'montreal expos',
+     'bill buckner',
+     'perfect game',
+     'brooklyn dodgers',
+     'joe dimaggio',
+     'fenway park',
+     'george brett',
+     'michael jordan',
+     'harry carey',
+     'dominican republic',
+     'jim palmer',
+     'walt hriniak',
+     'ted williams',
+     'ricky henderson',
+     'babe ruth',
+     'ty cobb',
+     'gaylord perry',
+     'harold baines',
+     'bart giamatti',
+     'stephen strasburg',
+     'miggy cabrera',
+     'carl yazstremski',
+     'the kid',
+     'ernie banks'
+];
+
+let randomIndex = Math.floor(Math.random() * mlbTrivia.length);
+let randomWord = mlbTrivia[randomIndex];
+
+let computerWord = new Word(randomWord);
+
+let requireNewWord = false;
